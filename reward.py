4e10aca17aca8231431c149c2981e502fb8db2f0
Thuy Nga Dinh <thudinh@2-E-2.42heilbronn.de>
2025-09-10 09:51:44 +0200

    re reward and logs

--- reward.py
+++ reward.py
@@ -1,112 +1,113 @@
 import math
 import numpy as np
 
-ideal_line =	[[ 3.48816359e+00,  1.42395903e+00],
-				[ 3.28587337e+00,  1.44977937e+00],
-				[ 3.07777896e+00,  1.46759960e+00],
-				[ 2.85560085e+00,  1.48058186e+00],
-				[ 2.59854854e+00,  1.49176047e+00],
-				[ 2.32173152e+00,  1.50016875e+00],
-				[ 2.03947493e+00,  1.50557821e+00],
-				[ 1.75371477e+00,  1.50845579e+00],
-				[ 1.46537601e+00,  1.50919586e+00],
-				[ 1.17496638e+00,  1.50817474e+00],
-				[ 8.82770265e-01,  1.50573766e+00],
-				[ 5.88992631e-01,  1.50218960e+00],
-				[ 2.93837257e-01,  1.49779074e+00],
-				[-2.47452957e-03,  1.49275711e+00],
-				[-3.05251770e-01,  1.48803851e+00],
-				[-6.08076941e-01,  1.48387722e+00],
-				[-9.10957831e-01,  1.48043188e+00],
-				[-1.21389817e+00,  1.47789871e+00],
-				[-1.51688999e+00,  1.47651031e+00],
-				[-1.81989907e+00,  1.47656341e+00],
-				[-2.12283774e+00,  1.47842281e+00],
-				[-2.42554462e+00,  1.48254293e+00],
-				[-2.72784680e+00,  1.48941442e+00],
-				[-3.02959544e+00,  1.49984481e+00],
-				[-3.33065842e+00,  1.51495465e+00],
-				[-3.60861788e+00,  1.51993536e+00],
-				[-3.87166146e+00,  1.50266787e+00],
-				[-4.11570237e+00,  1.45191090e+00],
-				[-4.33778523e+00,  1.35888649e+00],
-				[-4.52775133e+00,  1.20910898e+00],
-				[-4.68153504e+00,  1.01008606e+00],
-				[-4.78908787e+00,  7.72517935e-01],
-				[-4.84325484e+00,  5.16679091e-01],
-				[-4.84420716e+00,  2.60848924e-01],
-				[-4.79759665e+00,  1.56410125e-02],
-				[-4.70569282e+00, -2.11910797e-01],
-				[-4.56823148e+00, -4.14855503e-01],
-				[-4.38097570e+00, -5.82802859e-01],
-				[-4.16206873e+00, -7.23960632e-01],
-				[-3.91712774e+00, -8.40575540e-01],
-				[-3.65136893e+00, -9.36399126e-01],
-				[-3.36627812e+00, -1.01462628e+00],
-				[-3.07175975e+00, -1.07589826e+00],
-				[-2.77588033e+00, -1.12196601e+00],
-				[-2.47948952e+00, -1.15512054e+00],
-				[-2.18349357e+00, -1.17759764e+00],
-				[-1.88922739e+00, -1.19084253e+00],
-				[-1.59775905e+00, -1.19612871e+00],
-				[-1.30928350e+00, -1.19469098e+00],
-				[-1.02351668e+00, -1.18763271e+00],
-				[-7.40179466e-01, -1.17562039e+00],
-				[-4.59019524e-01, -1.15912452e+00],
-				[-1.79866373e-01, -1.13838887e+00],
-				[ 9.73013990e-02, -1.11328917e+00],
-				[ 3.72417561e-01, -1.08337244e+00],
-				[ 6.46367237e-01, -1.05051026e+00],
-				[ 9.14119051e-01, -1.01537627e+00],
-				[ 1.18155002e+00, -9.84802999e-01],
-				[ 1.44826104e+00, -9.63440528e-01],
-				[ 1.71372009e+00, -9.55807117e-01],
-				[ 1.97712017e+00, -9.67050172e-01],
-				[ 2.23719775e+00, -1.00344146e+00],
-				[ 2.49299695e+00, -1.06664619e+00],
-				[ 2.74620841e+00, -1.14409729e+00],
-				[ 2.99730481e+00, -1.23246737e+00],
-				[ 3.24812170e+00, -1.33067421e+00],
-				[ 3.49950348e+00, -1.41350329e+00],
-				[ 3.75074470e+00, -1.47175082e+00],
-				[ 3.99958092e+00, -1.49605082e+00],
-				[ 4.24112916e+00, -1.47700091e+00],
-				[ 4.46616539e+00, -1.40512866e+00],
-				[ 4.65927295e+00, -1.27231651e+00],
-				[ 4.79261710e+00, -1.07082102e+00],
-				[ 4.88454755e+00, -8.35910797e-01],
-				[ 4.93597969e+00, -5.73975669e-01],
-				[ 4.94831813e+00, -2.90757340e-01],
-				[ 4.92157409e+00,  5.71567223e-03],
-				[ 4.85684136e+00,  2.84735060e-01],
-				[ 4.76016615e+00,  5.38136730e-01],
-				[ 4.63574874e+00,  7.65405903e-01],
-				[ 4.48374073e+00,  9.62706619e-01],
-				[ 4.30548450e+00,  1.12544397e+00],
-				[ 4.10496449e+00,  1.24500029e+00],
-				[ 3.89713962e+00,  1.32851247e+00],
-				[ 3.69090567e+00,  1.38557296e+00],
-				[ 3.48816359e+00,  1.42395903e+00]]
+ideal_data = np.array([[ 3.4882,  1.424 ,  2.1187],
+				[ 3.2859,  1.4498,  2.196 ],
+				[ 3.0778,  1.4676,  2.2593],
+				[ 2.8556,  1.4806,  2.3101],
+				[ 2.5985,  1.4918,  2.3499],
+				[ 2.3217,  1.5002,  2.3797],
+				[ 2.0395,  1.5056,  2.4   ],
+				[ 1.7537,  1.5085,  2.4   ],
+				[ 1.4654,  1.5092,  2.4   ],
+				[ 1.175 ,  1.5082,  2.4   ],
+				[ 0.8828,  1.5057,  2.4   ],
+				[ 0.589 ,  1.5022,  2.4   ],
+				[ 0.2938,  1.4978,  2.4   ],
+				[-0.0025,  1.4928,  2.4   ],
+				[-0.3053,  1.488 ,  2.4   ],
+				[-0.6081,  1.4839,  2.4   ],
+				[-0.911 ,  1.4804,  2.4   ],
+				[-1.2139,  1.4779,  2.4   ],
+				[-1.5169,  1.4765,  2.4   ],
+				[-1.8199,  1.4766,  2.3943],
+				[-2.1228,  1.4784,  2.3681],
+				[-2.4255,  1.4825,  2.3203],
+				[-2.7278,  1.4894,  2.2491],
+				[-3.0296,  1.4998,  2.1537],
+				[-3.3307,  1.515 ,  2.0659],
+				[-3.6086,  1.5199,  1.9841],
+				[-3.8717,  1.5027,  1.8739],
+				[-4.1157,  1.4519,  1.6169],
+				[-4.3378,  1.3589,  1.3504],
+				[-4.5278,  1.2091,  1.4347],
+				[-4.6815,  1.0101,  1.5005],
+				[-4.7891,  0.7725,  1.5623],
+				[-4.8433,  0.5167,  1.5391],
+				[-4.8442,  0.2608,  1.5544],
+				[-4.7976,  0.0156,  1.6068],
+				[-4.7057, -0.2119,  1.54  ],
+				[-4.5682, -0.4149,  1.4421],
+				[-4.381 , -0.5828,  1.6196],
+				[-4.1621, -0.724 ,  1.7848],
+				[-3.9171, -0.8406,  1.942 ],
+				[-3.6514, -0.9364,  2.0221],
+				[-3.3663, -1.0146,  2.1003],
+				[-3.0718, -1.0759,  2.1874],
+				[-2.7759, -1.122 ,  2.2584],
+				[-2.4795, -1.1551,  2.3143],
+				[-2.1835, -1.1776,  2.3559],
+				[-1.8892, -1.1908,  2.3838],
+				[-1.5978, -1.1961,  2.3983],
+				[-1.3093, -1.1947,  2.4   ],
+				[-1.0235, -1.1876,  2.4   ],
+				[-0.7402, -1.1756,  2.4   ],
+				[-0.459 , -1.1591,  2.4   ],
+				[-0.1799, -1.1384,  2.4   ],
+				[ 0.0973, -1.1133,  2.4   ],
+				[ 0.3724, -1.0834,  2.4   ],
+				[ 0.6464, -1.0505,  2.3965],
+				[ 0.9141, -1.0154,  2.3857],
+				[ 1.1816, -0.9848,  2.3857],
+				[ 1.4483, -0.9634,  2.3857],
+				[ 1.7137, -0.9558,  2.3857],
+				[ 1.9771, -0.9671,  2.3613],
+				[ 2.2372, -1.0034,  2.2814],
+				[ 2.493 , -1.0666,  2.3201],
+				[ 2.7462, -1.1441,  2.2556],
+				[ 2.9973, -1.2325,  2.1699],
+				[ 3.2481, -1.3307,  2.0682],
+				[ 3.4995, -1.4135,  1.9562],
+				[ 3.7507, -1.4718,  1.8706],
+				[ 3.9996, -1.4961,  1.6905],
+				[ 4.2411, -1.477 ,  1.4575],
+				[ 4.4662, -1.4051,  1.2819],
+				[ 4.6593, -1.2723,  1.1287],
+				[ 4.7926, -1.0708,  1.3401],
+				[ 4.8845, -0.8359,  1.5299],
+				[ 4.936 , -0.574 ,  1.6572],
+				[ 4.9483, -0.2908,  1.6788],
+				[ 4.9216,  0.0057,  1.7031],
+				[ 4.8568,  0.2847,  1.7555],
+				[ 4.7602,  0.5381,  1.801 ],
+				[ 4.6357,  0.7654,  1.8268],
+				[ 4.4837,  0.9627,  1.6951],
+				[ 4.3055,  1.1254,  1.5492],
+				[ 4.105 ,  1.245 ,  1.7042],
+				[ 3.8971,  1.3285,  1.8407],
+				[ 3.6909,  1.3856,  1.9622],
+				[ 3.4882,  1.424 ,  2.0546],
+				[ 3.4882,  1.424 ,  2.1187]])
 
-prev_track_dir = []
-prev_steer = []
-prev_delta_steer = []
 
 def compute_direction(next_point, prev_point):
 	x_prev, y_prev = prev_point[0], prev_point[1]
 	x_next, y_next = next_point[0], next_point[1]
-	return (math.degrees(math.atan2(x_next - x_prev, y_next - y_prev)))
+	return (math.degrees(math.atan2(y_next - y_prev, x_next - x_prev)))
 
 def get_distance_from_ideal_line(x, y, ideal_point_1, ideal_point_2):
 	"""Calculates the distance from the ideal line defined by ideal_line."""
 	vector_ideal = np.array(ideal_point_2) - np.array(ideal_point_1)
 	vector_car = np.array([x, y]) - np.array(ideal_point_1)
-	dot_product = np.dot(vector_car, vector_ideal)
-	parallel_point = np.array(ideal_point_1) + (dot_product / np.dot(vector_ideal, vector_ideal)) * vector_ideal
-	distance = np.linalg.norm(np.array([x, y]) - parallel_point)
+	if np.linalg.norm(vector_ideal) < 1e-6:
+		distance = np.linalg.norm(vector_car)
+	else:
+		dot_product = np.dot(vector_car, vector_ideal)
+		parallel_point = np.array(ideal_point_1) + (dot_product / np.dot(vector_ideal, vector_ideal)) * vector_ideal
+		distance = np.linalg.norm(np.array([x, y]) - parallel_point)
 	return distance
 
-def	angle_diff_lookahead(params, n_lookahead=7):
+def	get_angle_diff_lookahead(ideal_line, params, n_lookahead):
 	x, y = params["x"], params["y"]
 	closest_idx = params['closest_waypoints'][1]
 	adj_idx = closest_idx + 1
@@ -132,43 +133,28 @@ def reward_function(params):
 	track_width = params['track_width']
 	is_offtrack = params['is_offtrack']
 
-	# --- Max speed based on action space ---
-	# should be changed according to the updated action space
-	max_speed_straight = 2.5
-	max_speed_soft_corner = 1.8
-	max_speed_sharp_corner = 1.2
-
 	# --- Base reward ---
 	if is_offtrack:
 		return 1e-3  # tiny reward if off track
 
-	reward = 1.0 
-
+	reward = 1.0
+	ideal_line = ideal_data[:, :2]
+	ideal_vel = ideal_data[:, 2]
 	prev_point = ideal_line[closest_waypoints[0]]
 	next_point = ideal_line[closest_waypoints[1]]
 	# --- Distance from center factor (smooth penalty) ---
 	distance_from_ideal = get_distance_from_ideal_line(params['x'], params['y'], prev_point, next_point)
-	distance_factor = max(0.0, 1 - 2 * distance_from_ideal / track_width)
+	distance_factor = max(1e-3, 1 - 2 * distance_from_ideal / track_width)
 
-	# Reward in range [0.5, 1.5]
-	reward *= (distance_factor + 0.5)
+	reward *= distance_factor * 1.5
 
 	track_direction = compute_direction(next_point, prev_point)
-	if steps == 1:
-		prev_track_dir.clear()
-		prev_steer.clear()
-		prev_delta_steer.clear()
-		prev_track_dir.append(heading)
-		prev_steer.append(steering)
 
-	# track_dir_diff = track_direction - prev_track_dir[-1]
-	# track_dir_diff = (track_dir_diff + 180.0) % 360.0 - 180.0
-
-	angle_diff_lookahead = angle_diff_lookahead(params, heading)
+	angle_diff_lookahead = get_angle_diff_lookahead(ideal_line, params, 1)
 	segment_type = 1
-	if (abs(angle_diff_lookahead) >= 5.0): #will enter a turn, second tier of velocity
+	if (angle_diff_lookahead >= 5): # will enter a LEFT turn
 		segment_type = 2
-	elif (abs(angle_diff_lookahead) >= 28.0): # sharper turn, third tier of velocity
+	elif (angle_diff_lookahead <= -5): # will enter a RIGHT turn
 		segment_type = 3
 	
 	# --- Heading should be aligned with the track_direction
@@ -176,45 +162,36 @@ def reward_function(params):
 	direction_diff = (direction_diff + 180.0) % 360.0 - 180.0
 	heading_factor = 1.0
 	if segment_type == 1:
-		heading_factor = max((1 - (direction_diff / 20))**1.2, 0.2)
+		heading_factor = max(1 - (direction_diff / 10), 0.0) ** 1.2
 	else:
-		heading_factor = max((1 - (direction_diff / 30))**1.2, 0.2)
+		heading_factor = max(1 - (direction_diff / 20), 0.0) ** 1.2
 	heading_factor *= 1.2
 	reward *= heading_factor
-
+	
 	# --- Speed factor ---
-	if segment_type == 1:
-		speed_factor = (speed / max_speed_straight) ** 2.0
-	elif segment_type == 2:  # Gentle curve
-		speed_factor = (speed / max_speed_soft_corner) ** 1.5
-	else:  # Sharp curve
-		speed_factor = (speed / max_speed_sharp_corner) ** 1.5
+	speed_0 = ideal_vel[closest_waypoints[0]]
+	speed_1 = ideal_vel[closest_waypoints[1]]
+	max_speed = (speed_0 + speed_1) / 2
+	if max_speed > 1e-3:
+		speed_factor = 1.0 - abs(speed - max_speed) / max_speed
+	else:
+		speed_factor = 0.1
+	speed_factor = max(speed_factor, 0.1)
 	reward *= max(speed_factor, 0.1) * 1.5
 
 	# --- Steering bonus ---
+	steering_factor = 1.0
 	if segment_type == 1:
 		steering_factor = max((1 - abs(steering) / 30.0), 0.1)
-	elif segment_type == 2:
-		if direction_diff <= 10.0:
-			tol = 5.0
-			delta_steer = abs(steering - prev_steer[-1])
-			if (len(prev_delta_steer) > 0):
-				delta_square_steer = abs(delta_steer - prev_delta_steer[-1])
-				s_jerk = max(0.1, 1 - delta_square_steer / (tol - 2))
-			s_rate = max(0.1, 1 - delta_steer / tol)
 	else:
-		if direction_diff <= 17.0:
-			tol = 8.0
-			delta_steer = abs(steering - prev_steer[-1])
-			if (len(prev_delta_steer) > 0):
-				delta_square_steer = abs(delta_steer - prev_delta_steer[-1])
-				s_jerk = max(0.1, 1 - delta_square_steer / (tol - 2))
-			s_rate = max(0.1, 1 - delta_steer / tol)
+		wrong_turn = steering * angle_diff_lookahead
+		if wrong_turn < 0.0:
+			steering_factor = min(max((abs(steering) / 20.0), 0.2), 0.6)
+
 	reward *= steering_factor * 1.2
 
 	# --- Progress bonus ---
-	progress_factor = (progress / steps) * 2.0  # scales by efficiency
+	progress_factor = (1.0 + progress / steps)  # scales by efficiency
 	reward += progress_factor
 
-	prev_track_dir.append(track_direction)
 	return float(reward)
